d9cbfd64cde8673b770294bcf0b1b230c83c330c
Paty Leyva <paty54323@gmail.com>
2026-02-06 20:36:11 +0000

    transformacion de textoss

--- 23-funcionesText.js
+++ 23-funcionesText.js
@@ -1,9 +1,21 @@
 'use strict'
 //Transformacion de textos
 var numero = 444;
-var texto = "Bienvenido";
+var texto = "Bienvenido a la";
 var texto2 = "Diass";
 
 var dato = numero.toString();
     dato = texto.toUpperCase();
 console.log( dato);
+
+//Calcular longitud 
+var nombre = "paty";
+console.log(nombre.length);
+
+//concatenar
+var textototal = texto.concat(" " + texto2);
+//var textototal= texto + texto2;
+console.log(textototal);
+
+var busqueda = texto.indexOf("la");
+console.log(busqueda);
