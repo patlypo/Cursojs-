000ca36d127a926c7410cfa99e10172d4af6b23c
Paty Leyva <paty54323@gmail.com>
2026-02-06 17:29:01 +0000

    update

--- 19-funciones.js
+++ 19-funciones.js
@@ -4,13 +4,13 @@
 function calculadora(numero1, numero2){
     console.log("Suma " + (numero1 + numero2));
     console.log("Resta " + (numero1 - numero2));
-    console.log("*******************")
+    console.log("*******************");
     //return "Hola soy la calculadora";
 
 }
 
 for (var i = 1; i<=10; i++){
-    console.log(i)
-    calculadora(i, 10);
+    console.log(i);
+    calculadora(i, 8);
 
 }
