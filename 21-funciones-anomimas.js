b3a48346133f326073e33847dbb9b05f7a1dad6b
Paty Leyva <paty54323@gmail.com>
2026-02-06 18:40:58 +0000

    ambito variables

--- 21-funciones-anomimas.js
+++ 21-funciones-anomimas.js
@@ -7,10 +7,18 @@ function sumame(numero1, numero2, sumaysumestra, sumapor2){
     return suma;
 
 }
-
+/** 
 sumame(5,7, function(dato){
     console.log("la suma es: " +dato);
 },function(dato){
+    console.log("La suma por 2 es: " + (dato*2));
+}
+);
+Esto es lo mismo que poner la fecla como abajo
+*/
+sumame(5,7, (dato) =>{
+    console.log("la suma es: " +dato);
+},(dato) =>{
     console.log("La suma por 2 es: " + (dato*2));
 }
 );
---
+++ 22-ambitoVariables.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function holamundo(texto){
+    console.log(texto);
+    console.log(numero.toString);
+}
+var numero=12;
+var texto = "Soy una variable global";
+holamundo(texto);
