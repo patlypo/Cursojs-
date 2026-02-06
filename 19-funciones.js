45326a0f0f417bf7f837522be516d2f979675ae1
Paty Leyva <paty54323@gmail.com>
2026-02-06 17:10:49 +0000

    subir archivos

---
+++ 19-funciones.js
@@ -0,0 +1,9 @@
+//FUNCIONES
+//Es una agrupación de reutilizable de un conjunto de instrucciones
+
+function calculadora(){
+    console.log("Hooa soy la calculadora");
+    console.log("soy yop");
+    //return "Hola soy la calculadora";
+
+}
