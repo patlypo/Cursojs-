ecbd9223701a29658ae7967c770ce341ee6da444
Paty Leyva <paty54323@gmail.com>
2026-02-06 17:37:41 +0000

    otros parametros

--- 19-funciones.js
+++ 19-funciones.js
@@ -1,16 +1,25 @@
 //FUNCIONES
 //Es una agrupación de reutilizable de un conjunto de instrucciones
 
-function calculadora(numero1, numero2){
-    console.log("Suma " + (numero1 + numero2));
-    console.log("Resta " + (numero1 - numero2));
-    console.log("*******************");
+function calculadora(numero1, numero2, mostrar = false){
+    if (mostrar = false) {
+        console.log("Suma " + (numero1 + numero2));
+        console.log("Resta " + (numero1 - numero2));
+        console.log("*******************");
+        
+    }else{
+        document.write("Suma " + (numero1 + numero2)) + "<br/>";
+        document.write("Resta " + (numero1 - numero2));
+        document.write("*******************");
+
+    }
     //return "Hola soy la calculadora";
-
 }
-
+calculadora(2,8);
+calculadora(2,4, true)
+/** 
 for (var i = 1; i<=10; i++){
     console.log(i);
     calculadora(i, 8);
 
-}
+}*/
