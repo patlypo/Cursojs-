ed62ea4ac7dba94b73923fc3c54521309a633205
Paty Leyva <paty54323@gmail.com>
2026-02-06 20:12:14 +0000

    transformacion de textos

---
+++ 23-funcionesText.js
@@ -0,0 +1,9 @@
+'use strict'
+//Transformacion de textos
+var numero = 444;
+var texto = "Bienvenido";
+var texto2 = "Diass";
+
+var dato = numero.toString();
+    dato = texto.toUpperCase();
+console.log( dato);
