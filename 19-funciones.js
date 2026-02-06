8b72dcae46c8eb447cd810ae0a713f0a975a2b95
Paty Leyva <paty54323@gmail.com>
2026-02-06 17:27:05 +0000

    ejercicio19

--- 19-funciones.js
+++ 19-funciones.js
@@ -1,9 +1,16 @@
 //FUNCIONES
 //Es una agrupación de reutilizable de un conjunto de instrucciones
 
-function calculadora(){
-    console.log("Hooa soy la calculadora");
-    console.log("soy yop");
+function calculadora(numero1, numero2){
+    console.log("Suma " + (numero1 + numero2));
+    console.log("Resta " + (numero1 - numero2));
+    console.log("*******************")
     //return "Hola soy la calculadora";
+
+}
+
+for (var i = 1; i<=10; i++){
+    console.log(i)
+    calculadora(i, 10);
 
 }
