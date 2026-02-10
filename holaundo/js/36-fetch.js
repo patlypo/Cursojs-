dcd23c53eda3533e91c7fd7ecaa716399c70a60b
Paty Leyva <paty54323@gmail.com>
2026-02-10 23:01:04 +0000

    fetch

--- holaundo/js/36-fetch.js
+++ holaundo/js/36-fetch.js
@@ -1,17 +1,21 @@
 'use strict'
 //Fetch (ajax) y peticiones a servicios/apis rest
 var div_usuarios = document.querySelector('#usuarios');
-var usuarios = [];
+//var usuarios = [];
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(data => data.json())//capturar los datos y se convierte a json
     .then(data =>{
-        usuarios = data;
+        //usuarios = data;
         console.log(usuarios);
-
-        usuarios.map((data,i)=>{
-            let nombre = document.createElement('h3');
-            nombre.innerHTML = i +" " + data.name + " ";
-            div_usuarios.appendChild(nombre);
-        });
+        listadousuarios(data);
     });
+
+
+function listadousuarios(usuarios){
+    usuarios.map((data, i) => {
+            let nombre = document.createElement('h3');
+            nombre.innerHTML = i +". " + data.name + " ";
+            div_usuarios.appendChild(nombre);
+    });      
+}
