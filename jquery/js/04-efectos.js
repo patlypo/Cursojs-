d65db154f4fcfe375ff29ecc0cfd2b1d873643f6
Paty Leyva <paty54323@gmail.com>
2026-02-12 19:24:00 +0000

    efectos

---
+++ jquery/js/04-efectos.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    $("#mostrar").click(function(){
+        $("#caja").show();
+    });
+    $("#ocultar").click(function(){
+        $("#caja").hide();
+    });
+});
