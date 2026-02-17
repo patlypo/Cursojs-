37d0001bff8845b7d439dc6593b8ae932fb37aa8
Paty Leyva <paty54323@gmail.com>
2026-02-17 22:48:51 +0000

    posts

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -6,4 +6,43 @@ $(document).ready(function(){
       responsive:true,
       pager:false
     });
+
+    //posts
+    var posts = [
+      {
+        title:'Prueba de titulo 1',
+        date: new Date(),
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Ullam, est sunt sint ipsum assumenda atque fuga at dolore? Aut iure blanditiis ipsam eveniet voluptatum dolorem?'
+      },
+      {
+        title:'Prueba de titulo 2',
+        date: new Date(),
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Ullam, est sunt sint ipsum assumenda atque fuga at dolore? Aut iure blanditiis ipsam eveniet voluptatum dolorem?'
+      
+      },
+      {
+        title:'Prueba de titulo 3',
+        date: new Date(),
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Ullam, est sunt sint ipsum assumenda atque fuga at dolore? Aut iure blanditiis ipsam eveniet voluptatum dolorem?'
+      
+      },
+      {
+        title:'Prueba de titulo 4',
+        date: new Date(),
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Ullam, est sunt sint ipsum assumenda atque fuga at dolore? Aut iure blanditiis ipsam eveniet voluptatum dolorem?Lorem ipsum dolor sit amet consectetur adipisicing elit.Ullam, est sunt sint ipsum assumenda atque fuga at dolore? Aut iure blanditiis ipsaLorem ipsum dolor sit amet consectetur adipisicing elit.Ullam, est sunt sint ipsum assumenda atque fuga at dolore? Aut iure blanditiis ipsaLorem ipsum dolor sit amet consectetur adipisicing elit.Ullam, est sunt sint ipsum assumenda atque fuga at dolore? Aut iure blanditiis ipsa'
+      }
+    ];
+
+    posts.forEach((item, index)=>{
+      var post = `
+        <article class="post">
+                    <h2>${item.title}</h2>
+                    <span class="date">${item.date}</span>
+                    <p>${item.content}
+                      </p>
+                    <a href="#" class="button-more">Leer más</a>
+                </article>
+      `;
+      $("#posts").append(post);
+    });
 });
